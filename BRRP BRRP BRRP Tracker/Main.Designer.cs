34ed0eb46877331be0f605e300233fed3f8940c9
Hayden (Technoguyfication) Andreyka <haydenandreyka@yahoo.com>
2015-07-20 09:04:27 -0700

    Initial Commit

---
+++ BRRP BRRP BRRP Tracker/Main.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace BRRP_BRRP_BRRP_Tracker
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(12, 12);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(546, 93);
+            this.PlayButton.TabIndex = 0;
+            this.PlayButton.Text = "Play";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 270);
+            this.Controls.Add(this.PlayButton);
+            this.Name = "Main";
+            this.Text = "BRRP BRRP BRRP Tracker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlayButton;
+    }
+}
+
---
+++ BRRP BRRP BRRP Tracker/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BRRP_BRRP_BRRP_Tracker
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
